c4c0fb05f3db8dd73c05f4df63596b869b0b444e
Adam Falaschi <adamfalaschi@gmail.com>
2025-08-14 23:28:35 +0000

    Criacao do primeiro programa em C

---
+++ Hello.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    //saida de dados em C
+    printf("Meu primeiro programa em C!\n");
+    printf("Agora estou pronto para os desafios em C!\n");
+
+    return 0;
+}
